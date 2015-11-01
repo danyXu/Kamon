ffa4e8b082c2f63c6a765868f51a223c90be240c
Danny <xuletter0927@126.com>
2015-11-01 11:38:26 +0800

    docker images

--- kamon-zipkin/docker/deploy/build.sh
+++ kamon-zipkin/docker/deploy/build.sh
@@ -7,3 +7,8 @@ for image in ${IMAGES[@]}; do
   docker build --no-cache -t "$PREFIX$image:master" .
   popd
 done
+
+
+# for a interuption, could you offet the docker image for this extension (kamon-zipkin) if you hava ?
+# because of running the build script has costed a lot of time and got the error result,i tried . 
+# waiting for your answer,thanks
